7d6542bc7a0f5276cfea37ecb62e8579a8975e74
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-08-12 00:07:00 -0300

    Commit inicial

---
+++ src/main/java/br/edu/infnet/cochitoapi/CochitoapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.cochitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochitoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochitoapiApplication.class, args);
+	}
+
+}
