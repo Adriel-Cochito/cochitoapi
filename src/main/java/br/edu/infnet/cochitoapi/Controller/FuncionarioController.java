f4d51243d0def3a0001f222eb03892f5c361bf1f
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-08-13 23:11:04 -0300

    Update FuncionarioController.java
    POST Cria Funcionario

--- src/main/java/br/edu/infnet/cochitoapi/Controller/FuncionarioController.java
+++ src/main/java/br/edu/infnet/cochitoapi/Controller/FuncionarioController.java
@@ -2,10 +2,13 @@ package br.edu.infnet.cochitoapi.Controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,12 @@ public class FuncionarioController {
 
 	public FuncionarioController(FuncionarioService funcionarioService) {
 		this.funcionarioService = funcionarioService;
+	}
+
+	@PostMapping
+	public ResponseEntity<Funcionario> incluirFuncionario(@RequestBody Funcionario funcionario) {
+		Funcionario funcionarioSalvo = funcionarioService.salvar(funcionario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(funcionarioSalvo);
 	}
 
 	@GetMapping
