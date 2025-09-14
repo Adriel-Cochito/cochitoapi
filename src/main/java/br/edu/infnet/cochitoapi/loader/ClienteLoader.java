2abf77e911c4b261c824914202081231a74d7e49
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-09-14 16:00:53 -0300

    Cliente com Endereço

--- src/main/java/br/edu/infnet/cochitoapi/loader/ClienteLoader.java
+++ src/main/java/br/edu/infnet/cochitoapi/loader/ClienteLoader.java
@@ -9,50 +9,67 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import br.edu.infnet.cochitoapi.model.domain.Cliente;
+import br.edu.infnet.cochitoapi.model.domain.Endereco;
 import br.edu.infnet.cochitoapi.model.service.ClienteService;
 
 @Component
 @Order(3)
 public class ClienteLoader implements ApplicationRunner {
-	
-	private final ClienteService clienteService;
-	
-	public ClienteLoader(ClienteService clienteService) {
-		this.clienteService = clienteService;
-	}
+    
+    private final ClienteService clienteService;
+    
+    public ClienteLoader(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		
-		FileReader arquivo = new FileReader("cliente.txt");
-		BufferedReader leitura = new BufferedReader(arquivo);
-		
-		String linha = leitura.readLine();
-		String[] campos = null;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        
+        FileReader arquivo = new FileReader("cliente.txt");
+        BufferedReader leitura = new BufferedReader(arquivo);
+        
+        String linha = leitura.readLine();
+        String[] campos = null;
 
-		System.out.println("---------------------------: ");
-		System.out.println("Clientes carregados: ");
-		
-		while(linha != null) {
-			
-			campos = linha.split(";");
-			
-			Cliente cliente = new Cliente();				
-			cliente.setNome(campos[0]);
-			cliente.setCpf(campos[1]);
-			cliente.setEmail(campos[2]);
-			cliente.setTelefone(campos[3]);
-			cliente.setFidelidade(campos[4]);
-			
-			clienteService.incluir(cliente);
+        System.out.println("---------------------------: ");
+        System.out.println("Clientes carregados: ");
+        
+        while(linha != null) {
+            
+            campos = linha.split(";");
+            
+            Cliente cliente = new Cliente();                
+            cliente.setNome(campos[0]);
+            cliente.setCpf(campos[1]);
+            cliente.setEmail(campos[2]);
+            cliente.setTelefone(campos[3]);
+            cliente.setFidelidade(campos[4]);
+            
+            // Criar e associar endereço (seguindo o padrão do FuncionarioLoader)
+            Endereco endereco = new Endereco();
+            endereco.setCep(campos[5]);
+            endereco.setLogradouro(campos[6]);
+            endereco.setComplemento(campos[7]);
+            endereco.setUnidade(campos[8]);
+            endereco.setBairro(campos[9]);
+            endereco.setLocalidade(campos[10]);
+            endereco.setUf(campos[11]);
+            endereco.setEstado(campos[12]);
+            
+            cliente.setEndereco(endereco);
+            
+            try {
+                clienteService.incluir(cliente);
+                System.out.println("  [OK] Cliente " + cliente.getNome() + " incluído com sucesso.");
+            } catch (Exception e) {
+                System.err.println("Erro ao incluir cliente " + cliente.getNome() + ": " + e.getMessage());
+            }
+            
+            linha = leitura.readLine();
+        }
+        
+        System.out.println("- Total de Clientes: " + clienteService.obterLista().size());
 
-			System.out.println(cliente);
-			
-			linha = leitura.readLine();
-		}
-		
-		System.out.println("- Total de Clientes: " + clienteService.obterLista().size());
-
-		leitura.close();
-	}
+        leitura.close();
+    }
 }
--- src/main/java/br/edu/infnet/cochitoapi/model/domain/Cliente.java
+++ src/main/java/br/edu/infnet/cochitoapi/model/domain/Cliente.java
@@ -1,43 +1,82 @@
 package br.edu.infnet.cochitoapi.model.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
 public class Cliente extends Pessoa {
 
-	@NotBlank(message = "A fidelidade é obrigatória.")
-	@Size(min = 3, max = 20, message = "Fidelidade deve ter entre 3 e 20 caracteres.")
-	private String fidelidade;
+    @NotBlank(message = "A fidelidade é obrigatória.")
+    @Size(min = 3, max = 20, message = "Fidelidade deve ter entre 3 e 20 caracteres.")
+    private String fidelidade;
 
-	// Construtor padrão
-	public Cliente() {}
-	
-	// Construtor com parâmetros
-	public Cliente(String nome, String email, String cpf, String telefone, String fidelidade) {
-		this.setNome(nome);
-		this.setEmail(email);
-		this.setCpf(cpf);
-		this.setTelefone(telefone);
-		this.fidelidade = fidelidade;
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("Cliente{%s, fidelidade='%s'}", super.toString(), fidelidade);
-	}
-	
-	@Override
-	public String obterTipo() {
-		return "Cliente";
-	}
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id")
+    @Valid
+    private Endereco endereco;
 
-	public String getFidelidade() {
-		return fidelidade;
-	}
+    // Construtor padrão
+    public Cliente() {}
 
-	public void setFidelidade(String fidelidade) {
-		this.fidelidade = fidelidade;
-	}
+    // Construtor com parâmetros básicos
+    public Cliente(String nome, String email, String cpf, String telefone, String fidelidade) {
+        this.setNome(nome);
+        this.setEmail(email);
+        this.setCpf(cpf);
+        this.setTelefone(telefone);
+        this.fidelidade = fidelidade;
+    }
+
+    // Construtor completo com endereço
+    public Cliente(String nome, String email, String cpf, String telefone, String fidelidade, Endereco endereco) {
+        this(nome, email, cpf, telefone, fidelidade);
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cliente{%s, fidelidade='%s', endereco=%s}", 
+                           super.toString(), fidelidade, endereco != null ? endereco.toString() : "null");
+    }
+
+    @Override
+    public String obterTipo() {
+        return "Cliente";
+    }
+
+    // Getters e Setters
+    public String getFidelidade() {
+        return fidelidade;
+    }
+
+    public void setFidelidade(String fidelidade) {
+        this.fidelidade = fidelidade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    // Método utilitário para verificar se tem endereço
+    public boolean temEndereco() {
+        return endereco != null;
+    }
+
+    // Método para obter endereço completo como string
+    public String getEnderecoCompleto() {
+        if (endereco == null) {
+            return "Endereço não informado";
+        }
+        return endereco.getLogradouro() + ", " + endereco.getBairro() + " - " + 
+               endereco.getLocalidade() + "/" + endereco.getUf() + " - CEP: " + endereco.getCep();
+    }
 }
